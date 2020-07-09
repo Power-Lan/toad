7fae7f029961fd89244b67a2197a5478232ef9c2
William MARTIN <william.martin@power-lan.com>
2020-07-09 16:19:49 +0200

    remove serial (it's the stlink serial, not mcu one)

--- src/TestSuite/STInfo.php
+++ src/TestSuite/STInfo.php
@@ -29,7 +29,6 @@ class STInfo implements Toad\TestSuiteInterface
           'descr',
           'pagesize',
           'chipid',
-          'serial'
         );
         foreach ($commands as $command) {
           $rc = $context->execute("st-info --" . $command, false);
